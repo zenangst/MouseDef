fe997f57dded7720055ebfc4b19be8e98877b18e
Christoffer Winterkvist <christoffer@winterkvist.com>
2020-05-15 16:16:17 +0200

    Add fail-safe for when user resizes and moves at the same time

--- Source/MonitorController.swift
+++ Source/MonitorController.swift
@@ -9,18 +9,28 @@ final class MonitorController {
 
   func start(_ handler: @escaping (State) -> Void) {
     flagMonitor = NSEvent.addGlobalMonitorForEvents(matching: .flagsChanged) { event in
+      let dragModifiers: NSEvent.ModifierFlags = [.function, .command]
+      let resizeModifiers: NSEvent.ModifierFlags = [.shift, .command]
+
       if event.modifierFlags.contains(.capsLock) || event.modifierFlags.contains(.option) ||
         event.modifierFlags.contains(.control) {
         handler(.ended)
         return
       }
 
-      if event.modifierFlags.contains([.function, .command]) {
+      // Avoid doing both resizing and moving at the same time.
+      // Reference: https://github.com/zenangst/MouseDef/issues/3
+      if event.modifierFlags.contains(dragModifiers) && event.modifierFlags.contains(resizeModifiers) {
+        handler(.ended)
+        return
+      }
+
+      if event.modifierFlags.contains(dragModifiers) {
         handler(.drag)
         return
       }
 
-      if event.modifierFlags.contains([.shift, .command]) {
+      if event.modifierFlags.contains(resizeModifiers) {
         handler(.resize)
         return
       }
